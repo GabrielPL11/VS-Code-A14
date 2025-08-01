d26e9643d97098f13e1a7ac2a621bd64c4ec9905
GabrielPL11 <gplgabrielgpl@gmail.com>
2025-07-31 18:53:14 -0600

    c1

--- Bitácora Hennigues/app.py
+++ Bitácora Hennigues/app.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    # Renderiza templates/index.html
     return render_template("index.html")
 
 if __name__ == "__main__":
-    # Ejecuta:  python app.py
+    # python app.py  →  http://127.0.0.1:5000
     app.run(debug=True, port=5000)
